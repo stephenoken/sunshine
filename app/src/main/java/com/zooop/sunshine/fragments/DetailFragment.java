852259bdb564d0d860abeae2da3e090136362fac
Stephen O'Kennedy <stephenoken@gmail.com>
2016-02-04 22:25:04 +0000

    "Can share the weatherer"
    ""

--- app/src/main/java/com/zooop/sunshine/fragments/DetailFragment.java
+++ app/src/main/java/com/zooop/sunshine/fragments/DetailFragment.java
@@ -3,9 +3,15 @@ package com.zooop.sunshine.fragments;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import com.zooop.sunshine.R;
@@ -15,7 +21,13 @@ import com.zooop.sunshine.R;
  */
 public class DetailFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
+
     public DetailFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -26,9 +38,32 @@ public class DetailFragment extends Fragment {
 
         Intent intent = getActivity().getIntent();
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
         }
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.detailfragment,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if (mShareActionProvider != null)
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        else
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+    }
+
+    private Intent createShareForecastIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,mForecastStr+FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
